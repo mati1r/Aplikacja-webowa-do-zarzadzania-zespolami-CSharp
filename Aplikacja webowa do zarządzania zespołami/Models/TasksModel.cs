d12ae09faad7eb18a3bcfb04a8e89be22c9be50e
mati1r <mati1r@wp.pl>
2023-08-08 14:21:49 +0200

    Modal + ajax

---
+++ Aplikacja webowa do zarządzania zespołami/Models/TasksModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aplikacja_webowa_do_zarządzania_zespołami.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime? FinishDate { get; set; }     
+        public string Priority { get; set; }
+        public string Status { get; set; }
+        public string? Feedback { get; set; }
+    }
+}
--- Aplikacja webowa do zarządzania zespołami/Pages/UserTasks.cshtml.cs
+++ Aplikacja webowa do zarządzania zespołami/Pages/UserTasks.cshtml.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aplikacja_webowa_do_zarządzania_zespołami.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
 {
     public class UserTasksModel : PageModel
     {
+        private readonly DatabaseContext _dbContext;
+
+        public UserTasksModel(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public List<Models.Task> TasksList;
+
         public void OnGet()
         {
+            TasksList = _dbContext.Task.ToList<Models.Task>();
+        }
 
+        public async Task<Models.Task> GetTaskAsync(int id)
+        {
+            return await _dbContext.Task.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+
+        public async Task<JsonResult> OnGetTaskJsonAsync(int id)
+        {
+            return new JsonResult(await GetTaskAsync(id));
         }
     }
 }
