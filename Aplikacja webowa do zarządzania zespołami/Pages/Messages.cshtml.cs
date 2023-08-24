198fe249ed177db42a9f7bd510a23056377a1432
mati1r <mati1r@wp.pl>
2023-08-24 14:19:51 +0200

    Wysyłanie wiadomości w trakcie (przesyłają sie wartości i waliduje się teamt oraz terść brak walidacji dla odbiorcy)

--- Aplikacja webowa do zarządzania zespołami/Pages/Messages.cshtml.cs
+++ Aplikacja webowa do zarządzania zespołami/Pages/Messages.cshtml.cs
@@ -2,6 +2,7 @@ using Aplikacja_webowa_do_zarządzania_zespołami.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
@@ -14,6 +15,7 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             _dbContext = dbContext;
             reciveMessagesList = new List<ReciveMessageView>();
             sendedMessagesList = new List<SendedMessageView>();
+            createMessagesView = new CreateMessageView();
             message = new Models.Message();
         }
 
@@ -36,6 +38,13 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             public string reciver_name { get; set; }
         }
 
+        public class CreateMessageView
+        {
+            public Models.Message message { get; set; }
+            public List<Models.User>? usersList { get; set; }
+            public string? messageUsers { get; set; }
+        }
+
         public List<ReciveMessageView> reciveMessagesList;
         public List<SendedMessageView> sendedMessagesList;
         public Message message;
@@ -45,6 +54,9 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
         public string data;
         public int? userId;
         public int? groupId;
+
+        [BindProperty(SupportsGet = true)]
+        public CreateMessageView createMessagesView { get; set; }
 
         private List<ReciveMessageView> GetMessages(int howManyRecords, int userId, int groupId)
         {
@@ -163,6 +175,47 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             sendedMessagesList = GetSendedMessageContent(id, (int)userId, (int)groupId);
 
             return Partial("Partials/_PartialSendedMessage", sendedMessagesList);
+        }
+        public PartialViewResult OnGetCreateMessage()
+        {
+            userId = HttpContext.Session.GetInt32(Key2);
+            groupId = HttpContext.Session.GetInt32(Key3);
+
+            createMessagesView.usersList = _dbContext.Users
+                .Where(g => g.Users_Groups
+                .Any(ug => ug.groups_group_id == groupId && ug.users_user_id != userId && ug.status == "aktywny"))
+                .ToList();
+
+            return Partial("Partials/_PartialCreateMessage", createMessagesView);
+        }
+
+        public IActionResult OnPostCreate()
+        {
+            data = HttpContext.Session.GetString(Key);
+            List<string> validationErrors = new List<string>();
+            if (!ModelState.IsValid)
+            {
+                var modelStateValidationErrors = ModelState.ToDictionary(ms => ms.Key,
+                   ms => ms.Value.Errors.Select(e => e.ErrorMessage).ToList());
+
+                foreach(var item in modelStateValidationErrors)
+                {
+                    Console.WriteLine(item.Key);
+                    foreach(var t in item.Value)
+                    {
+                        Console.WriteLine(t);
+                    }
+                }
+
+                return new JsonResult(modelStateValidationErrors);
+            }
+
+            //WALIDACJA DZIAŁA DLA TEMATU I WIADOMOŚCI, NIE DZIALA DLA ODBIORCY BO ODRAZU Z JAKIEGOŚ POWODU WYŚWIETLAŁ SIĘ BŁAD 
+            //DLA TEMATU I WIADOMOSCI SPRAWDZIC FORMATOWANIE A DLA ODBIORCY SPRAWDZIC CZY COS PRZYCHODZI, NASTEPNIE PRZEKONWERTOWAC TO NA LISTE INTÓW
+            //PRZYCHODZIĆ BĘDĄ np. "1,4,5" TAK WIĘC WARTOŚCI SĄ PO PROSTU ODZIELONE PRZECINKIEM, NALEŻY TO PRZEROBIĆ NA LISTE I SPARSOWAĆ NA INTY
+            //NASTĘPNIE SPRAWDZIĆ CZY TE WARTOŚCI SĄ NA LISCIE OSOB W GRUPIE, JEZELI TAK TO JEST GIT
+            Console.WriteLine(createMessagesView.message.content);
+            return new JsonResult("success");
         }
     }
 }
