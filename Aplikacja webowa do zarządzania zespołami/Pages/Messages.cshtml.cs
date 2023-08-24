15814378feaab7518875980377658c031af0c93c
mati1r <mati1r@wp.pl>
2023-08-24 11:39:57 +0200

    Działające wiadomości odebrane i wyslane

--- Aplikacja webowa do zarządzania zespołami/Pages/Messages.cshtml.cs
+++ Aplikacja webowa do zarządzania zespołami/Pages/Messages.cshtml.cs
@@ -12,12 +12,13 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
         public MessagesModel(DatabaseContext dbContext)
         {
             _dbContext = dbContext;
-            messagesList = new List<MessageView>();
+            reciveMessagesList = new List<ReciveMessageView>();
+            sendedMessagesList = new List<SendedMessageView>();
             message = new Models.Message();
         }
 
         //Subclass of messages that takes sender_name insted of sender_id so it can be shown to a user
-        public class MessageView
+        public class ReciveMessageView
         {
             public int message_id {  get; set; }
             public string topic { get; set; }
@@ -26,7 +27,17 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             public string sender_name { get; set; }
         }
 
-        public List<MessageView> messagesList;
+        public class SendedMessageView
+        {
+            public int message_id { get; set; }
+            public string topic { get; set; }
+            public string content { get; set; }
+            public DateTime send_date { get; set; }
+            public string reciver_name { get; set; }
+        }
+
+        public List<ReciveMessageView> reciveMessagesList;
+        public List<SendedMessageView> sendedMessagesList;
         public Message message;
         public const string Key = "_userType";
         public const string Key2 = "_userId";
@@ -35,20 +46,20 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
         public int? userId;
         public int? groupId;
 
-        private List<MessageView> GetMessages(int howManyRecords, int userId, int groupId)
+        private List<ReciveMessageView> GetMessages(int howManyRecords, int userId, int groupId)
         {
             return _dbContext.Messages
                 .Include(m => m.Users) //Include Users table by FK
                 .Where(m => m.Messages_Users.Any(mu => mu.users_user_id == userId) && m.groups_group_id == groupId) //Get elements that met the requaierments
                 .OrderByDescending(m => m.send_date)
                 .Take(howManyRecords)
-                .Select(m => new MessageView
+                .Select(m => new ReciveMessageView
                 {
                     message_id = m.message_id,
                     topic = m.topic,
                     content = m.content,
                     send_date = m.send_date,
-                    sender_name = m.Users.username // Pobierz nazwę nadawcy
+                    sender_name = m.Users.username
                 })
                 .ToList();
         }
@@ -59,22 +70,48 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             userId = HttpContext.Session.GetInt32(Key2);
             groupId = HttpContext.Session.GetInt32(Key3);
 
-            messagesList = GetMessages(10, (int)userId, (int)groupId);
+            reciveMessagesList = GetMessages(10, (int)userId, (int)groupId);
         }
 
-        public PartialViewResult OnGetMessagesView(int howMany)
+        public PartialViewResult OnGetReciveMessagesView(int howMany)
         {
             userId = HttpContext.Session.GetInt32(Key2);
             groupId = HttpContext.Session.GetInt32(Key3);
 
-            messagesList = GetMessages(howMany, (int)userId, (int)groupId);
+            reciveMessagesList = GetMessages(howMany, (int)userId, (int)groupId);
 
-            return Partial("Partials/_PartialMessagesView", messagesList);
+            return Partial("Partials/_PartialReciveMessagesView", reciveMessagesList);
+        }
+
+        public PartialViewResult OnGetSendedMessagesView(int howMany)
+        {
+            userId = HttpContext.Session.GetInt32(Key2);
+            groupId = HttpContext.Session.GetInt32(Key3);
+
+            //Get list of messages and nickname of one person that recived it (Group by and select statment)
+            sendedMessagesList = _dbContext.Messages
+                .Where(m => m.sender_id == userId && m.groups_group_id == groupId)
+                .OrderByDescending(m => m.send_date)
+                .SelectMany(m => m.Messages_Users, (m, mu) => new SendedMessageView
+                {
+                    message_id = m.message_id,
+                    topic = m.topic,
+                    content = m.content,
+                    send_date = m.send_date,
+                    reciver_name = mu.Users.username
+                })
+                .GroupBy(m => m.message_id)
+                .Select(m => m.First())
+                .Take(howMany)
+                .ToList();
+
+
+            return Partial("Partials/_PartialSendedMessagesView", sendedMessagesList);
         }
 
 
         //Get message and validate if user didn't changed id to some out of his scope or active group
-        private Message GetMessageContent(int messageId, int userId, int groupId)
+        private Message GetReciveMessageContent(int messageId, int userId, int groupId)
         {
             if(_dbContext.Messages.Where(m => m.Messages_Users.Any(mu => mu.users_user_id == userId) && m.groups_group_id == groupId && m.message_id == messageId).Count() > 0)
             {
@@ -85,13 +122,47 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             return falseMessage;
         }
 
-        public PartialViewResult OnGetMessage(int id)
+        public PartialViewResult OnGetReciveMessage(int id)
         {
             userId = HttpContext.Session.GetInt32(Key2);
             groupId = HttpContext.Session.GetInt32(Key3);
 
-            message = GetMessageContent(id, (int)userId, (int)groupId);
-            return Partial("Partials/_PartialMessage", message);
+            message = GetReciveMessageContent(id, (int)userId, (int)groupId);
+            return Partial("Partials/_PartialReciveMessage", message);
+        }
+
+        private List<SendedMessageView> GetSendedMessageContent(int messageId, int userId, int groupId)
+        {
+            List<SendedMessageView> messagesList = new List<SendedMessageView>();
+            if (_dbContext.Messages.Where(m => m.sender_id == userId && m.groups_group_id == groupId && m.message_id == messageId).Count() > 0)
+            {
+                messagesList = _dbContext.Messages
+                    .Where(m => m.message_id == messageId)
+                    .SelectMany(m => m.Messages_Users, (m, mu) => new SendedMessageView
+                    {
+                        message_id = m.message_id,
+                        topic = m.topic,
+                        content = m.content,
+                        send_date = m.send_date,
+                        reciver_name = mu.Users.username
+                    })
+                    .ToList();
+            }
+            else
+            {
+                messagesList.Add(new SendedMessageView { message_id = messageId, topic = "", content = "Błąd, widomość o tym id nie istnieje", send_date = DateTime.Now, reciver_name = "" });
+            }
+
+            return messagesList;
+        }
+
+        public PartialViewResult OnGetSendedMessage(int id)
+        {
+            userId = HttpContext.Session.GetInt32(Key2);
+            groupId = HttpContext.Session.GetInt32(Key3);
+            sendedMessagesList = GetSendedMessageContent(id, (int)userId, (int)groupId);
+
+            return Partial("Partials/_PartialSendedMessage", sendedMessagesList);
         }
     }
 }
