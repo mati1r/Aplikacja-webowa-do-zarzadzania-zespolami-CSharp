7081c4357fd7b353ed6ec995a52e0c504887dc3d
mati1r <mati1r@wp.pl>
2023-08-25 13:33:25 +0200

    Dzialajacy kalendarz (dostsowac wglad)

---
+++ Aplikacja webowa do zarządzania zespołami/Pages/Calendar.cshtml.cs
@@ -0,0 +1,50 @@
+using Aplikacja_webowa_do_zarządzania_zespołami.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
+{
+    public class CalendarModel : PageModel
+    {
+
+        private readonly DatabaseContext _dbContext;
+
+        public CalendarModel(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+            tasksList = new List<Models.Task>();
+        }
+
+        public List<Models.Task> tasksList;
+        private const string Key = "_userType";
+        private const string Key2 = "_userId";
+        private const string Key3 = "_groupId";
+        public string data;
+        public int? userId;
+        public int? groupId;
+
+        public void OnGet()
+        {
+            data = HttpContext.Session.GetString(Key);
+        }
+
+        public async Task<JsonResult> OnGetEventsAsync()
+        {
+            userId = HttpContext.Session.GetInt32(Key2);
+            groupId = HttpContext.Session.GetInt32(Key3);
+
+            var events = await _dbContext.Tasks
+                .Where(t => t.groups_group_id == groupId && t.users_user_id == userId)
+                .Select(t => new
+                {
+                    title = t.task_name,
+                    start = t.start_date.ToString("yyyy-MM-dd"),
+                    end = t.end_date.ToString("yyyy-MM-dd")
+                })
+                .ToListAsync();
+
+            return new JsonResult(events);
+        }
+    }
+}
