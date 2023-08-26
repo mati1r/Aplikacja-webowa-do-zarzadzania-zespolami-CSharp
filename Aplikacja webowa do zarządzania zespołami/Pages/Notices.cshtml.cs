4f11a42560dfec16016df6d4b8d64fcd2c9a9adb
mati1r <mati1r@wp.pl>
2023-08-26 11:48:42 +0200

    Dzialający widok z modalem

--- Aplikacja webowa do zarządzania zespołami/Pages/Notices.cshtml.cs
+++ Aplikacja webowa do zarządzania zespołami/Pages/Notices.cshtml.cs
@@ -36,7 +36,7 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
         {
             return _dbContext.Messages
                 .Include(m => m.Users) //Include Users table by FK
-                .Where(m => m.groups_group_id == groupId && m.notice == true) //Get elements that met the requaierments
+                .Where(m => m.groups_group_id == groupId && m.notice == true) //Get elements that meet the requaierments
                 .OrderByDescending(m => m.send_date)
                 .Select(m => new NoticeView
                 {
@@ -56,6 +56,42 @@ namespace Aplikacja_webowa_do_zarządzania_zespołami.Pages
             groupId = HttpContext.Session.GetInt32(Key3);
 
             noticesList = GetNotice(userId, groupId);  
+        }
+
+        public async Task<NoticeView> GetNoticeAsync(int id)
+        {
+            groupId = HttpContext.Session.GetInt32(Key3);
+            //Check if user didn't changed id to an id out of his scope or to an message insted of notice
+            if (_dbContext.Messages.Count(m => m.message_id == id && m.groups_group_id == groupId && m.notice == true) > 0)
+            {
+                return await _dbContext.Messages
+                .Include(m => m.Users)
+                .Where(m => m.message_id == id)
+                .Select(m => new NoticeView
+                {
+                    message_id = m.message_id,
+                    topic = m.topic,
+                    content = m.content,
+                    send_date = m.send_date,
+                    sender_name = m.Users.username
+                }).FirstAsync(m => m.message_id == id);
+            }
+            else
+            {
+                NoticeView emptyNotice = new NoticeView();
+                emptyNotice.send_date = DateTime.Now;
+                emptyNotice.message_id = 0;
+                emptyNotice.content = "Błąd nie znaleziono ogłoszenia";
+                emptyNotice.sender_name = "Błąd";
+                emptyNotice.topic = "Błąd";
+                return emptyNotice;
+            }
+        }
+
+
+        public async Task<JsonResult> OnGetNoticeJsonAsync(int id)
+        {
+            return new JsonResult(await GetNoticeAsync(id));
         }
     }
 }
