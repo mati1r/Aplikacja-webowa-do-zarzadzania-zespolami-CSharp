2c97dde08643893b8e70f4f9cd4d19ee2af2f085
mati1r <mati1r@wp.pl>
2023-09-06 14:09:59 +0200

    Dodano nowe ikonki oraz działający przycisk do ukrywania sidebara

---
+++ Aplikacja webowa do zarządzania zespołami/wwwroot/js/layout.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    $(document).ready(function () {
+        $("#sidebarBtn").click(function () {
+            let sidebar = $("#sidebar");
+            if (sidebar.css("width") === "280px") {
+                sidebar.css("width", "0");
+
+            } else {
+                sidebar.css("width", "280px");
+            }
+        });
+    });
+
+    //Zmiana klasy dla aktualnie wybranego elementu / strony
+    $('#navbar .nav-link').each(function () {
+        if ($(this).attr('href').toLowerCase() === location.pathname.toLowerCase()) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+});
